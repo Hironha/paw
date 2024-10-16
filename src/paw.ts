195d6d1211052e6d2ce8338e7ca57bf756697b50
Hironha <rafaelhirokato@gmail.com>
2024-10-16 13:21:53 -0300

    add refine

--- src/paw.ts
+++ src/paw.ts
@@ -1,8 +1,6 @@
 import * as Result from "./result.ts";
 
-export type PawError = {
-  message: string;
-};
+export type PawError = { message: string };
 
 export type PawResult<T> = Result.Result<T, PawError>;
 
@@ -35,82 +33,113 @@ export interface PawSchema<N extends string, T> extends PawParser<T> {
 }
 
 export interface PawOptional<T extends PawSchema<string, any>>
-  extends PawSchema<"opt", ReturnType<T["parse"]> | null | undefined> {}
+  extends PawSchema<"opt", ReturnType<T["parse"]> | null | undefined> {
+  refine<U>(fn: RefineFn<U>): PawOptional<T>;
+}
 
 export interface PawString extends PawSchema<"str", string> {
   optional(): PawOptional<PawString>;
+  refine<T>(fn: RefineFn<T>): PawString;
 }
 
 export interface PawNumber extends PawSchema<"num", number> {
-  optional(): PawOptional<PawNumber>;
+  int(): PawNumber;
   min(val: number): PawNumber;
   max(val: number): PawNumber;
-  int(): PawNumber;
+  optional(): PawOptional<PawNumber>;
+  refine<T>(fn: RefineFn<T>): PawNumber;
 }
 
 export interface PawBoolean extends PawSchema<"bool", boolean> {
   optional(): PawOptional<PawBoolean>;
+  refine<T>(fn: RefineFn<T>): PawBoolean;
 }
 
 export interface PawUnknown extends PawSchema<"unknown", unknown> {}
 
 export interface PawArray<T extends PawType> extends PawSchema<"array", PawInfer<T>[]> {
-  optional(): PawOptional<PawArray<T>>;
   min(size: number): PawArray<T>;
   max(size: number): PawArray<T>;
+  optional(): PawOptional<PawArray<T>>;
+  refine<U>(fn: RefineFn<U>): PawArray<T>;
 }
 
 export interface PawObject<T extends Record<string, PawType>>
   extends PawSchema<"object", ParsedPawObject<T>> {
   optional(): PawOptional<PawObject<T>>;
+  refine<U>(fn: RefineFn<U>): PawObject<T>;
 }
 
 export interface PawLiteral<T extends string> extends PawSchema<"literal", T> {
   optional(): PawOptional<PawLiteral<T>>;
 }
 
+export type RefineFn<T = any> = (val: unknown) => T;
+
 const OPT = "opt" as const;
 export class PawOptionalDecorator<T extends PawSchema<string, any>> implements PawOptional<T> {
   public readonly kind = OPT;
-  protected readonly parser: T;
+  private readonly _parser: T;
+  private _refine: RefineFn | undefined;
 
   constructor(parser: T) {
-    this.parser = parser;
+    this._parser = parser;
+  }
+
+  refine<U>(fn: RefineFn<U>): PawOptional<T> {
+    this._refine = fn;
+    return this;
   }
 
   parse(val: unknown): ReturnType<T["parse"]> | null | undefined {
-    if (val == null) {
-      return val;
+    const result = this.safeParse(val);
+    if (result.isErr()) {
+      throw new Error(result.value.message);
     }
-    return this.parser.parse(val);
+    return result.value;
   }
 
   safeParse(
     val: unknown,
   ): PawResult<ReturnType<T["parse"]> | null | undefined> {
+    if (this._refine) {
+      val = this._refine(val);
+    }
+
     if (val == null) {
       return Result.ok(val);
     }
-    return this.parser.safeParse(val);
+    return this._parser.safeParse(val);
   }
 }
 
 const STR = "str" as const;
 class PawStringParser implements PawString {
   public readonly kind = STR;
+  private _refine: RefineFn | undefined;
 
   optional(): PawOptional<PawString> {
     return new PawOptionalDecorator(this);
   }
 
+  refine<T>(fn: RefineFn<T>): PawString {
+    this._refine = fn;
+    return this;
+  }
+
   parse(val: unknown): string {
-    if (typeof val !== "string") {
-      throw new Error("Value is not a string");
+    const result = this.safeParse(val);
+    if (result.isErr()) {
+      throw new Error(result.value.message);
     }
-    return val;
+    return result.value;
   }
 
   safeParse(val: unknown): PawResult<string> {
+    if (this._refine) {
+      val = this._refine(val);
+    }
+
     if (typeof val !== "string") {
       return Result.err({ message: "Value is not a string" });
     }
@@ -124,6 +153,7 @@ class PawNumberParser implements PawNumber {
   private _int: boolean = false;
   private _min: number | undefined;
   private _max: number | undefined;
+  private _refine: RefineFn | undefined;
 
   int(): PawNumber {
     this._int = true;
@@ -144,6 +174,11 @@ class PawNumberParser implements PawNumber {
     return new PawOptionalDecorator(this);
   }
 
+  refine<T>(fn: RefineFn<T>): PawNumber {
+    this._refine = fn;
+    return this;
+  }
+
   parse(val: unknown): number {
     const result = this.safeParse(val);
     if (result.isErr()) {
@@ -154,6 +189,10 @@ class PawNumberParser implements PawNumber {
   }
 
   safeParse(val: unknown): PawResult<number> {
+    if (this._refine) {
+      val = this._refine(val);
+    }
+
     if (typeof val !== "number") {
       return Result.err({ message: "Value is not a number" });
     }
@@ -177,9 +216,15 @@ class PawNumberParser implements PawNumber {
 const BOOL = "bool" as const;
 class PawBooleanParser implements PawBoolean {
   public readonly kind = BOOL;
+  private _refine: RefineFn | undefined;
 
   optional(): PawOptional<PawBoolean> {
     return new PawOptionalDecorator(this);
+  }
+
+  refine<T>(fn: RefineFn<T>): PawBoolean {
+    this._refine = fn;
+    return this;
   }
 
   parse(val: unknown): boolean {
@@ -190,6 +235,10 @@ class PawBooleanParser implements PawBoolean {
   }
 
   safeParse(val: unknown): PawResult<boolean> {
+    if (this._refine) {
+      val = this._refine(val);
+    }
+
     if (typeof val !== "boolean") {
       return Result.err({ message: "Value is not a boolean" });
     }
@@ -216,6 +265,7 @@ class PawArrayParser<T extends PawType> implements PawArray<T> {
   private readonly _unit: T;
   private _max: number | undefined;
   private _min: number | undefined;
+  private _refine: RefineFn | undefined;
 
   constructor(unit: T) {
     this._unit = unit;
@@ -235,6 +285,11 @@ class PawArrayParser<T extends PawType> implements PawArray<T> {
     return new PawOptionalDecorator(this);
   }
 
+  refine<U>(fn: RefineFn<U>): PawArray<T> {
+    this._refine = fn;
+    return this;
+  }
+
   parse(val: unknown): PawInfer<T>[] {
     const result = this.safeParse(val);
     if (result.isErr()) {
@@ -244,6 +299,10 @@ class PawArrayParser<T extends PawType> implements PawArray<T> {
   }
 
   safeParse(val: unknown): PawResult<PawInfer<T>[]> {
+    if (this._refine) {
+      val = this._refine(val);
+    }
+
     if (!Array.isArray(val)) {
       return Result.err({ message: "Value is not an array" });
     }
@@ -270,14 +329,20 @@ class PawArrayParser<T extends PawType> implements PawArray<T> {
 const OBJECT = "object" as const;
 class PawObjectParser<T extends Record<string, PawType>> implements PawObject<T> {
   public readonly kind = OBJECT;
-  private readonly fields: T;
+  private readonly _fields: T;
+  private _refine: RefineFn | undefined;
 
   constructor(fields: T) {
-    this.fields = fields;
+    this._fields = fields;
   }
 
   optional(): PawOptional<PawObject<T>> {
     return new PawOptionalDecorator(this);
+  }
+
+  refine<U>(fn: RefineFn<U>): PawObject<T> {
+    this._refine = fn;
+    return this;
   }
 
   parse(val: unknown): ParsedPawObject<T> {
@@ -286,23 +351,27 @@ class PawObjectParser<T extends Record<string, PawType>> implements PawObject<T>
     }
 
     const obj: Record<string, unknown> = val as Record<string, unknown>;
-    for (const k in this.fields) {
+    for (const k in this._fields) {
       const v = obj[k];
-      this.fields[k]!.parse(v);
+      this._fields[k]!.parse(v);
     }
 
     return obj as ParsedPawObject<T>;
   }
 
   safeParse(val: unknown): PawResult<ParsedPawObject<T>> {
+    if (this._refine) {
+      val = this._refine(val);
+    }
+
     if (!val || typeof val !== "object") {
       throw new Error("Value is not an object");
     }
 
     const obj: Record<string, unknown> = val as Record<string, unknown>;
-    for (const k in this.fields) {
+    for (const k in this._fields) {
       const v = obj[k];
-      const parsed = this.fields[k]!.safeParse(v);
+      const parsed = this._fields[k]!.safeParse(v);
       if (parsed.isErr()) {
         return Result.err(parsed.value);
       }
--- src/paw_test.ts
+++ src/paw_test.ts
@@ -20,3 +20,26 @@ Deno.test("number parser should work", () => {
   assert(num.safeParse("test").isErr());
   assert(num.safeParse({}).isErr());
 });
+
+Deno.test("string refine should work", () => {
+  const str = paw
+    .string()
+    .refine((val) => (typeof val === "number" ? val.toString() : val));
+
+  assertEquals(str.parse("test"), "test");
+  assertEquals(str.parse(2), "2");
+  assert(str.safeParse(true).isErr());
+});
+
+Deno.test("optional refine should work", () => {
+  const optstr = paw
+    .string()
+    .refine((val) => (typeof val === "number" ? val.toString() : val))
+    .optional();
+
+  assertEquals(optstr.parse("test"), "test");
+  assertEquals(optstr.parse(2), "2");
+  assertEquals(optstr.parse(undefined), undefined);
+  assertEquals(optstr.parse(null), null);
+  assert(optstr.safeParse(true).isErr());
+});
